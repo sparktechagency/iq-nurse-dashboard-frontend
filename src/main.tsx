3fa6d886868777c89b2efb9005819dd97bc6039c
Yead191 <yead191@gmail.com>
2025-11-01 11:49:14 +0600

    modified title

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/routes.tsx";
-import { Provider } from "react-redux";
-import store from "./redux/store.ts";
-import { UserProvider } from "./provider/User.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/routes.tsx';
+import { Provider } from 'react-redux';
+import store from './redux/store.ts';
+import { UserProvider } from './provider/User.tsx';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <UserProvider>
-      <RouterProvider router={router} />
-      </UserProvider>
-    </Provider>
-  </StrictMode>
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <Provider store={store}>
+            <UserProvider>
+                <RouterProvider router={router} />
+            </UserProvider>
+        </Provider>
+    </StrictMode>,
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ export default {
             colors: {
                 primary: '#003877',
                 primaryText: '#474042',
-                shadeColor:"#F9F0EC",
-                overlay:"#F6F7F8"
+                shadeColor: '#F9F0EC',
+                overlay: '#F6F7F8',
             },
         },
     },
