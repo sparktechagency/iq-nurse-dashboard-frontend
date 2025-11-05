9a7f53c2cbd9b349457159961652a54352e3fc46
Yead191 <yead191@gmail.com>
2025-11-05 10:15:50 +0600

    server fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: 3000,
-        host: '10.10.7.95',
-        open: true,
-    },
+    // server: {
+    //     port: 3000,
+    //     host: '10.10.7.95',
+    //     open: true,
+    // },
 });
